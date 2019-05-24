d01c8b88c6c5f062ac33af182b73652c78fc1072
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-24 02:02:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = Hash.new
+  data.each do |attribute, type|
+    type.each do |type2,name|
+      name.each do |names|
+        if pigeon_list.keys.include?(names)&&pigeon_list[names].include?(attribute)
+          pigeon_list[names][attribute].push(type2.to_s)
+        elsif pigeon_list.keys.include?(names)&&!(pigeon_list[names].include?(attribute))
+          pigeon_list[names][attribute] = [type2.to_s]
+        else
+      pigeon_list[names] = Hash.new
+      pigeon_list[names][attribute] = [type2.to_s]
+    end
+    end
+  end
+end
+  pigeon_list
 end
